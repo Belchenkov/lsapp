8417f3f03695337b08e7ad50817b3d3c6a24e5e9
Belchenkov <u608110@gmail.com>
2017-06-20 13:31:01 +0300

    Models & Databas Migration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'LSAPP') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    
+    <link rel="stylesheet" href="http://bootswatch.com/sandstone/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+</head>
+<body>
+
+    @include('inc/navbar')    
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostController');
